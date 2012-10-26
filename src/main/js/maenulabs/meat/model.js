2589629097e6f667ab80ffacbac9098d0b324dab
Manuel Leuenberger <manuel.leu@students.unibe.ch>
2012-10-26 22:22:06 +0200

    reorganized

---
+++ src/main/js/maenulabs/meat/model.js
@@ -0,0 +1,9 @@
+/**
+ * JavaScript model for meat.
+ *
+ * @author Manuel Leuenberger
+ */
+
+var maenulabs = maenulabs || {};
+maenulabs.meat = maenulabs.meat || {};
+maenulabs.meat.model = maenulabs.meat.model || {};
