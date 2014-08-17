93943aff06d40251d0a56d3c8cdf42fedc799ace
Manuel Leuenberger <maenulabs@gmx.ch>
2014-08-17 16:17:23 +0200

    gruntify and use type lib

--- src/main/js/meat/ast.js
+++ src/main/js/meat/ast.js
@@ -7,187 +7,144 @@
 var meat = meat || {};
 meat.ast = meat.ast || {};
 
-meat.ast.Node = new Class(
-	Object,
-	function () {
-		Object.apply(this, []);
-	},
-	{
-		accept: function (visitor) {
-			throw new Error('abstract method');
-		}
-	});
+var Type = ch.maenulabs.type.Type;
 
-meat.ast.StatementsNode = new Class(
-	meat.ast.Node,
-	function (statements) {
-		meat.ast.Node.apply(this, []);
+meat.ast.Node = new Type(Object, {
+	accept: function(visitor) {
+		throw new Error('abstract method');
+	}
+});
+
+meat.ast.LiteralNode = new Type(meat.ast.Node);
+
+meat.ast.MessageNode = new Type(meat.ast.Node, {
+	initialize: function(selector) {
+		this.base('initialize')();
+		this.selector = selector;
+	},
+	accept: function(visitor) {
+		visitor.visitMessageNode(this);
+	}
+});
+
+meat.ast.StatementNode = new Type(meat.ast.Node);
+
+meat.ast.StatementsNode = new Type(meat.ast.Node, {
+	initialize: function(statements) {
+		this.base('initialize')();
 		this.statements = statements;
 	},
-	{
-		accept: function (visitor) {
-			visitor.visitStatementsNode(this);
-		}
-	});
+	accept: function(visitor) {
+		visitor.visitStatementsNode(this);
+	}
+});
 
-meat.ast.StatementNode = new Class(
-	meat.ast.Node,
-	function () {
-		meat.ast.Node.apply(this, []);
-	},
-	{
-		
-	});
-
-meat.ast.MessageSendNode = new Class(
-	meat.ast.StatementNode,
-	function (expression, message) {
-		meat.ast.StatementNode.apply(this, []);
+meat.ast.MessageSendNode = new Type(meat.ast.StatementNode, {
+	initialize: function(expression, message) {
+		this.base('initialize')();
 		this.expression = expression;
 		this.message = message;
 	},
-	{
-		accept: function (visitor) {
-			visitor.visitMessageSendNode(this);
-		}
-	});
+	accept: function(visitor) {
+		visitor.visitMessageSendNode(this);
+	}
+});
 
-meat.ast.CommentNode = new Class(
-	meat.ast.StatementNode,
-	function (lines) {
-		meat.ast.StatementNode.apply(this, []);
+meat.ast.CommentNode = new Type(meat.ast.StatementNode, {
+	initialize: function(lines) {
+		this.base('initialize')();
 		this.lines = lines;
 	},
-	{
-		accept: function (visitor) {
-			visitor.visitCommentNode(this);
-		}
-	});
+	accept: function(visitor) {
+		visitor.visitCommentNode(this);
+	}
+});
 
-meat.ast.LiteralNode = new Class(
-	meat.ast.Node,
-	function () {
-		meat.ast.Node.apply(this, []);
-	},
-	{
-		
-	});
-
-meat.ast.VariableNode = new Class(
-	meat.ast.LiteralNode,
-	function (identifier) {
-		meat.ast.LiteralNode.apply(this, []);
+meat.ast.VariableNode = new Type(meat.ast.LiteralNode, {
+	initialize: function(identifier) {
+		this.base('initialize')();
 		this.identifier = identifier;
 	},
-	{
-		accept: function (visitor) {
-			visitor.visitVariableNode(this);
-		}
-	});
+	accept: function(visitor) {
+		visitor.visitVariableNode(this);
+	}
+});
 
-meat.ast.BlockNode = new Class(
-	meat.ast.LiteralNode,
-	function (statements) {
-		meat.ast.LiteralNode.apply(this, []);
+meat.ast.BlockNode = new Type(meat.ast.LiteralNode, {
+	initialize: function(statements) {
+		this.base('initialize')();
 		this.statements = statements;
 	},
-	{
-		accept: function (visitor) {
-			visitor.visitBlockNode(this);    
-		}
-	});
+	accept: function(visitor) {
+		visitor.visitBlockNode(this);
+	}
+});
 
-meat.ast.CharacterNode = new Class(
-	meat.ast.LiteralNode,
-	function (character) {
-		meat.ast.LiteralNode.apply(this, []);
+meat.ast.CharacterNode = new Type(meat.ast.LiteralNode, {
+	initialize: function(character) {
+		this.base('initialize')();
 		this.character = character;
 	},
-	{
-		accept: function (visitor) {
-			visitor.visitCharacterNode(this);
-		}
-	});
+	accept: function(visitor) {
+		visitor.visitCharacterNode(this);
+	}
+});
 
-meat.ast.StringNode = new Class(
-	meat.ast.LiteralNode,
-	function (string) {
-		meat.ast.LiteralNode.apply(this, []);
+meat.ast.StringNode = new Type(meat.ast.LiteralNode, {
+	initialize: function(string) {
+		this.base('initialize')();
 		this.string = string;
 	},
-	{
-		accept: function (visitor) {
-			visitor.visitStringNode(this);
-		}
-	});
+	accept: function(visitor) {
+		visitor.visitStringNode(this);
+	}
+});
 
-meat.ast.NumberNode = new Class(
-	meat.ast.LiteralNode,
-	function (number) {
-		meat.ast.LiteralNode.apply(this, []);
+meat.ast.NumberNode = new Type(meat.ast.LiteralNode, {
+	initialize: function(number) {
+		this.base('initialize')();
 		this.number = number;
 	},
-	{
-		accept: function (visitor) {
-			visitor.visitNumberNode(this);
-		}
-	});
+	accept: function(visitor) {
+		visitor.visitNumberNode(this);
+	}
+});
 
-meat.ast.ListNode = new Class(
-	meat.ast.LiteralNode,
-	function (elements) {
-		meat.ast.LiteralNode.apply(this, []);
+meat.ast.ListNode = new Type(meat.ast.LiteralNode, {
+	initialize: function(elements) {
+		this.base('initialize')();
 		this.elements = elements;
 	},
-	{
-		accept: function (visitor) {
-			visitor.visitListNode(this);
-		}
-	});
+	accept: function(visitor) {
+		visitor.visitListNode(this);
+	}
+});
 
-meat.ast.MessageNode = new Class(
-	meat.ast.Node,
-	function (selector) {
-		meat.ast.Node.apply(this, []);
-		this.selector = selector;
+meat.ast.UnaryMessageNode = new Type(meat.ast.MessageNode, {
+	initialize: function(selector) {
+		this.base('initialize')(selector);
 	},
-	{
-		accept: function (visitor) {
-			visitor.visitMessageNode(this);
-		}
-	});
+	accept: function(visitor) {
+		visitor.visitUnaryMessageNode(this);
+	}
+});
 
-meat.ast.UnaryMessageNode = new Class(
-	meat.ast.MessageNode,
-	function (selector) {
-		meat.ast.MessageNode.apply(this, [selector]);
-	},
-	{
-		accept: function (visitor) {
-			visitor.visitUnaryMessageNode(this);
-		}
-	});
-
-meat.ast.BinaryMessageNode = new Class(
-	meat.ast.MessageNode,
-	function (selector, argument) {
-		meat.ast.MessageNode.apply(this, [selector, argument]);
+meat.ast.BinaryMessageNode = new Type(meat.ast.MessageNode, {
+	initialize: function(selector, argument) {
+		this.base('initialize')(selector);
 		this.argument = argument;
 	},
-	{
-		accept: function (visitor) {
-			visitor.visitBinaryMessageNode(this);
-		}
-	});
+	accept: function(visitor) {
+		visitor.visitBinaryMessageNode(this);
+	}
+});
 
-meat.ast.KeywordMessageNode = new Class(
-	meat.ast.MessageNode,
-	function (selector, arguments) {
-		meat.ast.MessageNode.apply(this, [selector, arguments]);
+meat.ast.KeywordMessageNode = new Type(meat.ast.MessageNode, {
+	initialize: function(selector, arguments) {
+		this.base('initialize')(selector);
 		this.arguments = arguments
 	},
-	{
-		accept: function (visitor) {
-			visitor.visitKeywordMessageNode(this);
-		}
-	});
+	accept: function(visitor) {
+		visitor.visitKeywordMessageNode(this);
+	}
+});
--- src/main/js/meat/compiler.js
+++ src/main/js/meat/compiler.js
@@ -7,108 +7,102 @@
 var meat = meat || {};
 meat.compiler = meat.compiler || {};
 
-meat.compiler.Compiler = new Class(
-	Object,
-	function () {
-		Object.apply(this, arguments);
-	},
-	{
-		compile: function (ast) {
-			var visitor = new meat.compiler.AstVisitor();
-			ast.accept(visitor);
-			return visitor.source;
-		}
-	});
+var Type = ch.maenulabs.type.Type;
 
-meat.compiler.AstVisitor = new Class(
-	Object,
-	function () {
-		Object.apply(this, arguments);
+meat.compiler.Compiler = new Type(Object, {
+	compile: function(ast) {
+		var visitor = new meat.compiler.AstVisitor();
+		ast.accept(visitor);
+		return visitor.source;
+	}
+});
+
+meat.compiler.AstVisitor = new Type(Object, {
+	initialize: function() {
 		this.source = '';
 	},
-	{
-		visitStatementsNode: function (node) {
-			for (var i = 0; i < node.statements.length - 1; i++) {
-				node.statements[i].accept(this);
-				this.source += ';\n';
-			}
-			this.source += 'return ';
-			node.statements[node.statements.length - 1].accept(this);
+	visitStatementsNode: function(node) {
+		for (var i = 0; i < node.statements.length - 1; i++) {
+			node.statements[i].accept(this);
 			this.source += ';\n';
-		},
-		visitCommentNode: function (node) {
-			this.source += 'new meat.model.Comment(new meat.model.List([';
-			for (var i = 0; i < node.lines.length - 1; i++) {
-				this.source += 'new meat.model.String(\'';
-				this.source += node.lines[i];
-				this.source += '\'), ';
-			}
-			this.source += 'new meat.model.String(\'';
-			this.source += node.lines[node.lines.length - 1];
-			this.source += '\')';
-			this.source += '])';
-		},
-		visitMessageSendNode: function (node) {
-			this.source += 'interpreter.interpret(';
-			node.message.accept(this);
-			this.source += ', ';
-			node.expression.accept(this);
-			this.source += ', context)';
-		},
-		visitVariableNode: function (node) {
-			this.source += 'new meat.model.Variable(new meat.model.String(\'';
-			this.source += node.identifier;
-			this.source += '\'))';
-		},
-		visitBlockNode: function (node) {
-			this.source += 'new meat.model.Block(function (context) {\n';
-			node.statements.accept(this);
-			this.source += '})';
-		},
-		visitCharacterNode: function (node) {
-			this.source += 'new meat.model.Character(\'';
-			this.source += node.character
-			this.source += '\')';
-		},
-		visitStringNode: function (node) {
-			this.source += 'new meat.model.String(\'';
-			this.source += node.string
-			this.source += '\')';
-		},
-		visitNumberNode: function (node) {
-			this.source += 'new meat.model.Number('
-			this.source += node.number
-			this.source += ')';
-		},
-		visitListNode: function (node) {
-			this.source += 'new meat.model.List([';
-			for (var i = 0; i < node.elements.length; i++) {
-				if (i > 0) {
-					this.source += ', ';
-				}
-				node.elements[i].accept(this);
-			}
-			this.source += '])';
-		},
-		visitUnaryMessageNode: function (node) {
-			this.source += 'new meat.model.String(\'' + node.selector + '\')';
-			this.source += ', new meat.model.List([]), this';
-		},
-		visitBinaryMessageNode: function (node) {
-			this.source += 'new meat.model.String(\'' + node.selector + '\')';
-			this.source += ', new meat.model.List([';
-			node.argument.accept(this);
-			this.source += ']), this';
-		},
-		visitKeywordMessageNode: function (node) {
-			this.source += 'new meat.model.String(\'' + node.selector + '\')';
-			this.source += ', new meat.model.List([';
-			for (var i = 0; i < node.arguments.length; i++) {
-				if (i > 0) {
-					this.source += ', ';
-				}
-				node.arguments[i].accept(this);
-			}
-			this.source += ']), this';
 		}
-	});
+		this.source += 'return ';
+		node.statements[node.statements.length - 1].accept(this);
+		this.source += ';\n';
+	},
+	visitCommentNode: function(node) {
+		this.source += 'new meat.model.Comment(new meat.model.List([';
+		for (var i = 0; i < node.lines.length - 1; i++) {
+			this.source += 'new meat.model.String(\'';
+			this.source += node.lines[i];
+			this.source += '\'), ';
+		}
+		this.source += 'new meat.model.String(\'';
+		this.source += node.lines[node.lines.length - 1];
+		this.source += '\')';
+		this.source += '])';
+	},
+	visitMessageSendNode: function(node) {
+		this.source += 'interpreter.interpret(';
+		node.message.accept(this);
+		this.source += ', ';
+		node.expression.accept(this);
+		this.source += ', context)';
+	},
+	visitVariableNode: function(node) {
+		this.source += 'new meat.model.Variable(new meat.model.String(\'';
+		this.source += node.identifier;
+		this.source += '\'))';
+	},
+	visitBlockNode: function(node) {
+		this.source += 'new meat.model.Block(function (context) {\n';
+		node.statements.accept(this);
+		this.source += '})';
+	},
+	visitCharacterNode: function(node) {
+		this.source += 'new meat.model.Character(\'';
+		this.source += node.character
+		this.source += '\')';
+	},
+	visitStringNode: function(node) {
+		this.source += 'new meat.model.String(\'';
+		this.source += node.string
+		this.source += '\')';
+	},
+	visitNumberNode: function(node) {
+		this.source += 'new meat.model.Number('
+		this.source += node.number
+		this.source += ')';
+	},
+	visitListNode: function(node) {
+		this.source += 'new meat.model.List([';
+		for (var i = 0; i < node.elements.length; i++) {
+			if (i > 0)  {
+				this.source += ', ';
+			}
+			node.elements[i].accept(this);
+		}
+		this.source += '])';
+	},
+	visitUnaryMessageNode: function(node) {
+		this.source += 'new meat.model.String(\'' + node.selector + '\')';
+		this.source += ', new meat.model.List([]), this';
+	},
+	visitBinaryMessageNode: function(node) {
+		this.source += 'new meat.model.String(\'' + node.selector + '\')';
+		this.source += ', new meat.model.List([';
+		node.argument.accept(this);
+		this.source += ']), this';
+	},
+	visitKeywordMessageNode: function(node) {
+		this.source += 'new meat.model.String(\'' + node.selector + '\')';
+		this.source += ', new meat.model.List([';
+		for (var i = 0; i < node.arguments.length; i++) {
+			if (i > 0)  {
+				this.source += ', ';
+			}
+			node.arguments[i].accept(this);
+		}
+		this.source += ']), this';
+	}
+});
